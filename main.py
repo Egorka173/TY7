150a7e1bd777a806f6a4129d019180a46b7146b0
Egorka173 <fedorovegor152008@gmail.com>
2020-11-10 11:48:58 +1000

    татарин

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
-import sys 
+import sys
 
-W, H = 1000, 800
+W, H = 900, 600
 collide = False
 n = 0
 
@@ -24,14 +24,14 @@ screen = pygame.display.set_mode((W, H))
 font = pygame.font.Font(None, 32)
 # создаем поверхность размером в  2-а раза больше радиуса круга и вкл. альфа-канал
 surface = pygame.Surface((circle_radius * 2, circle_radius * 2), pygame.SRCALPHA)
-# на созданной поверхности рисуем круг желтого цвета 
+# на созданной поверхности рисуем круг желтого цвета
 pygame.draw.circle(surface, YELLOW, (circle_radius, circle_radius), circle_radius)
 # находим рект у поверхности
 rect1 = surface.get_rect()
 
 clock = pygame.time.Clock()
 FPS = 100
-speed_x, speed_y = 5, 1
+speed_x, speed_y = 10, 10
 
 
 ball = pygame.image.load('ball.png')
@@ -44,24 +44,25 @@ while True:
             sys.exit(0)
         elif e.type == pygame.MOUSEMOTION:
             # circle_pos = e.pos
-            rect1.center = e.pos  
+            rect1.center = e.pos
 
     ball_rect = ball_rect.move(speed_x, speed_y)
     if ball_rect.left < 0 or ball_rect.right > W:
         speed_x = -speed_x
     if ball_rect.top < 0 or ball_rect.bottom > H:
         speed_y = -speed_y
-
+    ball_rect
     screen.fill(BG)
     COLOR = RED if collide else BLUE
     # rect1 = pygame.draw.circle(screen, YELLOW, circle_pos, circle_radius)
     rect2 = pygame.draw.rect(screen, COLOR, (rect_pos, rect_size))
+    rect3 = pygame.draw.rect(screen, RED if collide else BLUE, (ball_rect))
     screen.blit(surface, rect1)
 
-    if rect1.colliderect(rect2):
+    if rect3.colliderect(rect2):
         collide = True
         if COLOR == BLUE:
-            n += 1
+            n += 100000000
     else:
         collide = False
 
